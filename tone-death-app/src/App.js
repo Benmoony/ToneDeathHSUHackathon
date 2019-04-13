e636d5d1397aabcfbfdcda9fcedf64a1939548b5
Benmoony <benmiller92@gmail.com>
2019-04-12 21:29:16 -0700

    Added Create-React-Build
    Initialized Stuff for the react app

---
+++ tone-death-app/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            This is the start of the Team Tone Death HSU Hackathon Application
+          </p>
+          <p>Lets Get Building!</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
